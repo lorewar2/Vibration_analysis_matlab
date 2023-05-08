b1b32cfc36d8d3a8f6ea84e162f28002770edd6c
minindu0 <mw8511@live.com>
2023-05-07 20:08:21 -0500

    simple displacement check

--- signal_processing.m
+++ signal_processing.m
@@ -22,37 +22,82 @@ elseif axis == 'z'
     acc = z_acc;
 end
 
-% high pass filter
-passed_acc = highpass(acc, pass_band_frequency, sampling_frequency);
+% intrapolate the acceleration to small steps
+time_small_step = 0:0.001:1;
+intrapolated_acc = interp1(time, acc, time_small_step);
 
-% fast discrete fourier
-N = length(acc);
-xdft_acc = fft(passed_acc);
-xdft_acc = xdft_acc(1:N/2+1);
+% velocity calculation
+vel = [0];
+for c = 2:length(time_small_step)
+    time_gap = time_small_step(c) - time_small_step(c-1);
+    acc_sum = intrapolated_acc(c-1) + intrapolated_acc(c);
+    vel(end + 1) = 0.5 * time_gap * acc_sum; 
+end
 
-% power spectral density
-psdx = (1/(sampling_frequency*N)) * abs(xdft_acc).^2;
-psdx(2:end-1) = 2*psdx(2:end-1);
-freq = 0:sampling_frequency/N:sampling_frequency/2;
+% displacement calculation
+disp = [0];
+for c = 2:length(time_small_step)
+    time_gap = time_small_step(c) - time_small_step(c-1);
+    vel_sum = vel(c-1) + vel(c);
+    disp(end + 1) = 0.5 * time_gap * vel_sum; 
+end
 
 % plot the graph
 figure;
 tiledlayout(3,1)
 nexttile
-plot(time, acc)
-grid on
-title('time domain plot')
+plot(time_small_step, intrapolated_acc)
+title('Acceleration intrapolated, 1000 steps per second')
 xlabel('time (s)')
-ylabel('accer (ms-1)')
+ylabel('accer (ms-2)')
 nexttile
-plot(time, passed_acc)
-grid on
-title('high-pass filtered time domain plot')
+plot(time_small_step, vel)
+title('Velocity graph')
 xlabel('time (s)')
-ylabel('accer (ms-1)')
+ylabel('velocity (ms-1)')
 nexttile
-plot(freq,pow2db(psdx))
-grid on
-title("Periodogram Using FFT")
-xlabel("Frequency (Hz)")
-ylabel("Power/Frequency (dB/Hz)")
+plot(time_small_step, disp)
+title('Displacement graph')
+xlabel('time (s)')
+ylabel('displacement (m)')
+
+[max_disp, max_disp_index] = max(disp);
+acc_at_max_disp = intrapolated_acc(max_disp_index);
+fprintf('max_displacement = %im \nacc at max dis = %im\n', max_disp, acc_at_max_disp)
+
+
+
+%
+% % high pass filter
+% passed_acc = highpass(acc, pass_band_frequency, sampling_frequency);
+% % fast discrete fourier
+% N = length(acc);
+% xdft_acc = fft(passed_acc);
+% xdft_acc = xdft_acc(1:N/2+1);
+% 
+% % power spectral density
+% psdx = (1/(sampling_frequency*N)) * abs(xdft_acc).^2;
+% psdx(2:end-1) = 2*psdx(2:end-1);
+% freq = 0:sampling_frequency/N:sampling_frequency/2;
+% 
+% % plot the graph
+% figure;
+% tiledlayout(3,1)
+% nexttile
+% plot(time, acc)
+% grid on
+% title('time domain plot')
+% xlabel('time (s)')
+% ylabel('accer (ms-1)')
+% nexttile
+% plot(time, passed_acc)
+% grid on
+% title('high-pass filtered time domain plot')
+% xlabel('time (s)')
+% ylabel('accer (ms-1)')
+% nexttile
+% plot(freq,pow2db(psdx))
+% grid on
+% title("Periodogram Using FFT")
+% xlabel("Frequency (Hz)")
+% ylabel("Power/Frequency (dB/Hz)")
